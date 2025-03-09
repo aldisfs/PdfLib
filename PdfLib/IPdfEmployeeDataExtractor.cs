f32140fb7820c7b7a2e1f5758b6bfffc47659e91
Soojeong <soojlee0106@naver.com>
2025-03-09 20:42:12 +0900

    feat: 인터페이스 추가

---
+++ PdfLib/IPdfEmployeeDataExtractor.cs
@@ -0,0 +1,15 @@
+﻿namespace PdfLib
+{
+    /// <summary>
+    /// 원천징수영수증 PDF 파일에서 직원 데이터를 추출하는 기능을 제공하는 인터페이스입니다.
+    /// </summary>
+    public interface IPdfEmployeeDataExtractor
+    {
+        /// <summary>
+        /// 원천징수영수증 PDF 파일에서 직원 데이터를 추출합니다.
+        /// </summary>
+        /// <param name="filePath">원천징수영수증 PDF 파일의 경로</param>
+        /// <returns>추출된 직원 데이터를 포함하는 <see cref="PdfEmployeeData"/> 객체</returns>
+        PdfEmployeeData ExtractPdfEmployeeData(string filePath);
+    }
+}
