606c82775988170ae34751acb9d74dcecbe42c10
Soojeong <soojlee0106@naver.com>
2025-03-09 20:57:05 +0900

    fix: debug 모드 추가

--- PdfLib/IPdfEmployeeDataExtractor.cs
+++ PdfLib/IPdfEmployeeDataExtractor.cs
@@ -9,7 +9,8 @@
         /// 원천징수영수증 PDF 파일에서 직원 데이터를 추출합니다.
         /// </summary>
         /// <param name="filePath">원천징수영수증 PDF 파일의 경로</param>
+        /// <param name="isDebug">디버깅 여부를 나타내는 플래그 (true일 경우 디버깅 정보 출력)</param>
         /// <returns>추출된 직원 데이터를 포함하는 <see cref="PdfEmployeeData"/> 객체</returns>
-        PdfEmployeeData ExtractPdfEmployeeData(string filePath);
+        PdfEmployeeData ExtractPdfEmployeeData(string filePath, bool isDebug = false);
     }
 }
--- PdfLib/PdfEmployeeDataExtractor2024.cs
+++ PdfLib/PdfEmployeeDataExtractor2024.cs
@@ -14,11 +14,12 @@ namespace PdfLib
         /// 지정된 PDF 파일에서 직원 데이터를 추출합니다.
         /// </summary>
         /// <param name="filePath">PDF 파일 경로</param>
+        /// <param name="isDebug">디버깅 여부를 나타내는 플래그 (true일 경우 디버깅 정보 출력)</param>
         /// <returns>추출된 직원 데이터를 포함하는 <see cref="PdfEmployeeData"/> 객체</returns>
         /// <exception cref="Exception">
         /// 잘못된 파일이거나, 직원 정보(이름, 주민등록번호 등)가 가려지거나, 기준년도가 작년이 아닌 경우 예외가 발생합니다.
         /// </exception>
-        public PdfEmployeeData ExtractPdfEmployeeData(string filePath)
+        public PdfEmployeeData ExtractPdfEmployeeData(string filePath, bool isDebug = false)
         {
             List<List<string>> firstTableData = PdfManager.ImportPdfToTable(filePath, 1);
 
@@ -26,11 +27,13 @@ namespace PdfLib
                .Select(row => row.Select(cell => Regex.Replace(cell, @"\s+", "")).ToList())
                .ToList();
 
-            // 디버깅용
-            //foreach (var row in firstTableData)
-            //{
-            //    Console.WriteLine(string.Join("|", row));
-            //}
+            if (isDebug)
+            {
+                foreach (var row in firstTableData)
+                {
+                    Console.WriteLine(string.Join("|", row));
+                }
+            }
 
             string name = firstTableData
                   .FirstOrDefault(row => row.Any(cell => cell.Contains("⑥") && row.Any(c => c.Contains("⑦"))))?
@@ -44,14 +47,17 @@ namespace PdfLib
                 .Substring(0, 8)
                 .Replace("-", "") ?? "";
 
-            if (name.Length == 0 || uid.Length == 0)
+            if (!isDebug)
             {
-                throw new Exception("잘못된 파일입니다.");
-            }
+                if (name.Length == 0 || uid.Length == 0)
+                {
+                    throw new Exception("잘못된 파일입니다.");
+                }
 
-            if (name.Contains("*") || uid.Contains("*"))
-            {
-                throw new Exception("이름이나 주민번호가 가려져 직원 정보를 알 수 없습니다.");
+                if (name.Contains("*") || uid.Contains("*"))
+                {
+                    throw new Exception("이름이나 주민번호가 가려져 직원 정보를 알 수 없습니다.");
+                }
             }
 
             int baseYear = firstTableData
@@ -61,14 +67,17 @@ namespace PdfLib
                 .Select(cell => int.TryParse(cell.Trim().Substring(0, 4), out int value) ? value : 0)
                 .FirstOrDefault() ?? 0;
 
-            if (baseYear == 0)
+            if (!isDebug)
             {
-                throw new Exception("잘못된 파일입니다.");
-            }
+                if (baseYear == 0)
+                {
+                    throw new Exception("잘못된 파일입니다.");
+                }
 
-            if (baseYear != DateTime.Now.Year - 1)
-            {
-                throw new Exception("작년 기준년도의 원천징수영수증이 아닙니다.");
+                if (baseYear != DateTime.Now.Year - 1)
+                {
+                    throw new Exception("작년 기준년도의 원천징수영수증이 아닙니다.");
+                }
             }
 
             decimal totalSum = firstTableData
@@ -101,15 +110,17 @@ namespace PdfLib
                .Select(cell => int.TryParse(cell.Trim().Replace(",", ""), out int value) ? value : 0)
                .Sum() ?? 0;
 
-            // 디버깅용
-            //Console.WriteLine("------------");
-            //Console.WriteLine($"name: {name}");
-            //Console.WriteLine($"uid: {uid}");
-            //Console.WriteLine($"baseYear: {baseYear}");
-            //Console.WriteLine($"totalSum: {totalSum}");
-            //Console.WriteLine($"untaxedTotalSum: {untaxedTotalSum}");
-            //Console.WriteLine($"previousTaxPaid: {previousTaxPaid}");
-            //Console.WriteLine($"deductibleTax: {deductibleTax}");
+            if (isDebug)
+            {
+                Console.WriteLine("------------");
+                Console.WriteLine($"name: {name}");
+                Console.WriteLine($"uid: {uid}");
+                Console.WriteLine($"baseYear: {baseYear}");
+                Console.WriteLine($"totalSum: {totalSum}");
+                Console.WriteLine($"untaxedTotalSum: {untaxedTotalSum}");
+                Console.WriteLine($"previousTaxPaid: {previousTaxPaid}");
+                Console.WriteLine($"deductibleTax: {deductibleTax}");
+            }
 
             List<List<string>> secondTableData = PdfManager.ImportPdfToTable(filePath, 2, 3, 25);
 
@@ -117,11 +128,13 @@ namespace PdfLib
                .Select(row => row.Select(cell => Regex.Replace(cell, @"\s+", "")).ToList())
                .ToList();
 
-            // 디버깅용
-            //foreach (var row in secondTableData)
-            //{
-            //    Console.WriteLine(string.Join("|", row));
-            //}
+            if (isDebug)
+            {
+                foreach (var row in secondTableData)
+                {
+                    Console.WriteLine(string.Join("|", row));
+                }
+            }
 
             int nationalPensionRowIndex = secondTableData
                     .FindIndex(row => row.Any(cell => cell.Contains("국민연금보험료")));
@@ -192,16 +205,18 @@ namespace PdfLib
             int preCalculatedSalary = Convert.ToInt32(totalSum + untaxedTotalSum - previousTaxPaid -
                 (nationalPension + publicOfficialPension + soldierPension + privateSchoolPension + postalPension + healthInsurance + employmentInsurance));
 
-            // 디버깅용
-            //Console.WriteLine("------------");
-            //Console.WriteLine($"nationalPension: {nationalPension}");
-            //Console.WriteLine($"publicOfficialPension: {publicOfficialPension}");
-            //Console.WriteLine($"soldierPension: {soldierPension}");
-            //Console.WriteLine($"privateSchoolPension: {privateSchoolPension}");
-            //Console.WriteLine($"postalPension: {postalPension}");
-            //Console.WriteLine($"healthInsurance: {healthInsurance}");
-            //Console.WriteLine($"employmentInsurance: {employmentInsurance}");
-            //Console.WriteLine($"preCalculatedSalary: {preCalculatedSalary}");
+            if (isDebug)
+            {
+                Console.WriteLine("------------");
+                Console.WriteLine($"nationalPension: {nationalPension}");
+                Console.WriteLine($"publicOfficialPension: {publicOfficialPension}");
+                Console.WriteLine($"soldierPension: {soldierPension}");
+                Console.WriteLine($"privateSchoolPension: {privateSchoolPension}");
+                Console.WriteLine($"postalPension: {postalPension}");
+                Console.WriteLine($"healthInsurance: {healthInsurance}");
+                Console.WriteLine($"employmentInsurance: {employmentInsurance}");
+                Console.WriteLine($"preCalculatedSalary: {preCalculatedSalary}");
+            }
 
             return new PdfEmployeeData(name, uid, baseYear, preCalculatedSalary, deductibleTax);
         }
